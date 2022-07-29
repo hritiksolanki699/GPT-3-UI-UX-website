bf94fa663c6017ba0253ed5746cf44167e01c5f0
Hritik <hritiksolanki699@gmail.com>
2022-07-29 18:49:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Navbar, Brand, CTA} from './components'
+import { Blog, Features, Footer, Header, WhatGPT3, Possibility } from './containers'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className= "App">
+     <div className='gradient__bg'>
+      <Navbar />
+      <Header />
+     </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
